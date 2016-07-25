8d6c57f14dcd5886f378a20da364504e21c4195e
Oleg Kutkov <kuvkov@yandex.ru>
2016-07-25 14:54:10 +0300

    separate directory for all shell scripts

---
+++ scripts/cam_arch_manager.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+####
+
+next_date=`date +"%d.%m.%Y" --date="next day"`
+
+archdir=/storage/web/archive
+archpath=${archdir}/${next_date}
+
+if [ ! -d ${archpath} ]
+then
+	echo "Creating archive directory " ${archpath}
+	mkdir -p ${archpath}
+fi
+
+#####
+
+arch_dirs=`ls /storage/web/archive/`
+
+for dir in ${arch_dirs}
+do
+	echo "Checking archive directory "${dir}
+
+	dir_name_to_date=`echo ${dir} | awk -F "." '{print $3"-"$2"-"$1}'`
+
+	days_between_days=`date -d @$(( $(date +%s) - $(date -d "$dir_name_to_date" +%s) )) -u +'%d'`
+
+	if [ $days_between_days -le 0 ]; then
+		continue
+	fi
+
+	if [ $days_between_days -ge 3 ]
+	then
+		echo ${dir}" is "$days_between_days" days old and will be removed"
+
+		rm -fr ${archdir}/${dir}
+	fi
+
+	echo -e "\n"
+
+done
+
---
+++ scripts/cam_to_arch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp /storage/web/cam2.jpg /storage/web/archive/`date +"%d.%m.%Y"`/`date +"%H.%M__%d.%m.%Y"`.jpg
