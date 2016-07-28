07bfd0702b64219a9d2b96b7582550ba65c8aa1e
Oleg Kutkov <kuvkov@yandex.ru>
2016-07-28 13:50:49 +0300

    simple script to get cpu temp and put in to db

---
+++ scripts/cpu_temp_to_db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+put_to_db()
+{
+	temp=$1
+
+	echo "INSERT INTO cpu_sensor (temperature) VALUES ($temp);" | tee | mysql -uallsky -pallsky allsky
+}
+
+cpu_temp=`cat /sys/class/thermal/thermal_zone0/temp`
+cpu_temp=`bc <<< "scale=2; $cpu_temp/1000"`
+
+put_to_db $cpu_temp
+
