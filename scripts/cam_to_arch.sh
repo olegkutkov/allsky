68167f39139b5223dee9e2b38fbb7c938be6b0eb
Oleg Kutkov <elenbert@gmail.com>
2017-12-03 23:42:27 +0300

    put to arch images from the specified camera only

--- scripts/cam_to_arch.sh
+++ scripts/cam_to_arch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CAM_NAME=$1
+
 # simple copy current image to archive
-cp /storage/web/cam1.jpg /storage/web/archive/`date +"%d.%m.%Y"`/cam1/`date +"%H.%M__%d.%m.%Y"`.jpg
-cp /storage/web/cam2.jpg /storage/web/archive/`date +"%d.%m.%Y"`/cam2/`date +"%H.%M__%d.%m.%Y"`.jpg
+cp -v /storage/web/${CAM_NAME}.jpg /storage/web/archive/`date +"%d.%m.%Y"`/${CAM_NAME}/`date +"%H.%M__%d.%m.%Y"`.jpg
+cp -v /storage/web/${CAM_NAME}.jpg /storage/web/archive/`date +"%d.%m.%Y"`/${CAM_NAME}/`date +"%H.%M__%d.%m.%Y"`.jpg
