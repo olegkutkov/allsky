975788074ec615188ca390fbda23a9410a60b9da
Aleksandra Nekhaeva <lesanra@yandex.ru>
2017-12-13 13:02:47 +0300

    refactoring

--- web/js/gallery.js
+++ web/js/gallery.js
@@ -1,41 +1,42 @@
-function openModal() {
-  document.getElementById('modal').style.display = "block";
-}
+$(document).ready(function () {
+    var slideIndex = 0;
+    var $modal = $('#modal');
 
-function closeModal() {
-  document.getElementById('modal').style.display = "none";
-}
+    showSlides(slideIndex);
 
-var slideIndex = 1;
-showSlides(slideIndex);
+    $modal.modal({
+        remote: url,
+        refresh: true
+    });
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    function showSlides(n) {
+        var $slides = $('.imgSlides');
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+        if (n > $slides.length) {slideIndex = 0}
+        if (n < 1) {slideIndex = $slides.length - 1}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("imgSlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+        $slides.css({display: 'none'});
+        $slides[slideIndex].style.display = 'block';
+    }
 
-$('#modal').modal({
-    remote: url,
-    refresh: true
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function openModal() {
+        $modal.show();
+    }
+
+    function closeModal() {
+        $modal.hide();
+    }
+
+    window.plusSlides = plusSlides;
+    window.currentSlide = currentSlide;
+    window.openModal = openModal;
+    window.closeModal = closeModal;
 });
