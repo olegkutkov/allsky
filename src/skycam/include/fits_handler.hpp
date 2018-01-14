259c771700e8d34337b3902fabf933c5ce22af6d
Oleg Kutkov <elenbert@gmail.com>
2018-01-15 01:57:54 +0300

    subs for the fits handler

---
+++ src/skycam/include/fits_handler.hpp
@@ -0,0 +1,38 @@
+/*
+ QHY camera tool
+
+ Copyright 2016  Oleg Kutkov <kutkov.o@yandex.ru>
+ Based on original QHY code from https://github.com/qhyccd-lzr
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+*/
+
+#ifndef FITS_HANDLER_HPP
+#define FITS_HANDLER_HPP
+
+#include <string>
+
+class FitsHandler
+{
+	FitsHandler(const std::string &filename);
+	~FitsHandler();
+
+	bool lLoadData();
+	bool SaveData();
+	bool ReleaseData();
+
+	void operator-();
+
+};
+
+#endif 
+
