421344e664b0ea163f2289aeba1aac8887ed8a1d
Oleg Kutkov <elenbert@gmail.com>
2018-01-24 10:54:51 +0300

    corrections for the expo values

--- src/ephem/ephemp_dump.py
+++ src/ephem/ephemp_dump.py
@@ -9,7 +9,7 @@ import config
 
 ephem_data = {}
 
-
+sunrise_tw_angle = '-4'
 civil_tw_angle = '-6'
 astro_tw_angle = '-18'
 
@@ -54,9 +54,12 @@ else:
 	is_night = True
 	time_of_day_str = 'Night'
 
-	observer.horizon = astro_tw_angle
+#	observer.horizon = astro_tw_angle
+	observer.horizon = sunrise_tw_angle
 	next_sunrise_time = ephem.localtime(observer.next_rising(sun_ephem, use_center=True))
 #	prev_astro_tw_start_time = ephem.localtime(observer.previous_setting(sun_ephem, use_center=True))
+
+	observer.horizon = civil_tw_angle
 
 	night_start_next = ephem.localtime(observer.next_setting(sun_ephem, use_center=True))
 
@@ -66,9 +69,10 @@ else:
 		if tdelta <= 5400:
 			print 'Today night start at ', night_start_next
 			astro_tw_start_time = night_start_next
+		else:
+			astro_tw_start_time = ephem.localtime(observer.previous_setting(sun_ephem, use_center=True))
 	else:
 		astro_tw_start_time = ephem.localtime(observer.previous_setting(sun_ephem, use_center=True))
-			
 
 if is_night:
 	print 'Night start', astro_tw_start_time.strftime("%H:%M:%S")
@@ -85,11 +89,6 @@ else:
 	ephem_data['civil_twilight_start'] = str(next_civil_tw_start_time)
 	ephem_data['astro_twilight_start'] = str(next_astro_tw_start_time)
 
-print ephem_data
-
-with open(config.JSON_PATH, 'w') as fp:
-    json.dump(ephem_data, fp)
-
 ## moon
 
 observer.horizon = '-0:34'
@@ -105,18 +104,22 @@ next_moon_setting = ephem.localtime(observer.next_setting(moon_ephem))
 previous_full = ephem.localtime(ephem.previous_full_moon(observer.date))
 next_full = ephem.localtime(ephem.next_full_moon(observer.date))
 
-#print prev_moon_rising
+print 'prev_moon_rising', prev_moon_rising
 #print prev_moon_transit
-print next_moon_transit
-#print next_moon_setting
+#print next_moon_transit
+print 'next_moon_setting', next_moon_setting
 
-print '#'
+#print '#'
 
 #print previous_full
 #print next_full
 
 names = ['Waxing Crescent', 'Waxing Gibbous',
          'Waning Gibbous', 'Waning Crescent']
+
+ephem_data['moon_correction'] = False
+
+moon_ephem.compute(observer)
 
 if is_night:
 	if localtime > prev_moon_rising and localtime < next_moon_setting:
@@ -132,11 +135,38 @@ if is_night:
 
 		if quarter == 1 or quarter == 2:
 			print 'Moon is to bright'
+			ephem_data['moon_correction'] = True
+			ephem_data['moon_alt_deg'] = float(moon_ephem.alt) * 57.2957795
 
-		sunlon = ephem.Ecliptic(sun_ephem).lon
-		moonlon = ephem.Ecliptic(moon_ephem).lon
+			ephem_data['moon_phase_name'] = names[quarter]
 
-		tau = 2.0 * ephem.pi
+			moon_az = float(moon_ephem.az) * 57.2957795
+	
+			moon_place = ''
+
+			if moon_az > 0 and moon_az <= 45:
+				moon_place = 'North-East'
+			elif moon_az > 45 and moon_az <= 90:
+				moon_place = 'East'
+			elif moon_az > 90 and moon_az <= 135:
+				moon_place = 'South-East'
+			elif moon_az > 135 and moon_az <= 180:
+				moon_place = 'South'
+			elif moon_az > 180 and moon_az <= 225:
+				moon_place = 'South-West'
+			elif moon_az > 225 and moon_az <= 270:
+				moon_place = 'West'
+			elif moon_az > 270 and moon_az <= 315:
+				moon_place = 'North-West'
+			else:
+				moon_place = 'North ?'
+
+			ephem_data['moon_place'] = moon_place
+
+print ephem_data
+
+with open(config.JSON_PATH, 'w') as fp:
+    json.dump(ephem_data, fp)
 
 
 #print next_full
@@ -144,7 +174,6 @@ if is_night:
 
 
 #sun_ephem.compute(observer)
-#moon_ephem.compute(observer)
 
 #print ephem.localtime(observer.next_rising(moon_ephem, use_center=True))
 
