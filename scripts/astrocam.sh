5cad402ee99a778ce5782e3f7d960503ade7a551
Oleg Kutkov <elenbert@gmail.com>
2018-06-01 11:02:45 +0300

    new version of the astrocam script with full support of the FITS files and data

--- scripts/astrocam.sh
+++ scripts/astrocam.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-CAM1_CHECK_RUN_FILE='/storage/ephemdata/cam1_blackbox'
-CAM1_DARK_IMAGE="/storage/web/dark.jpg"
+CAM1_CHECK_RUN_FILE="/storage/ephemdata/cam1_blackbox"
+CAM1_LAST_IMAGE="/storage/fits/last.fits"
+CAM1_PREV_IMAGE="/storage/fits/prev.fits"
+CAM1_DARK_IMAGE="/storage/fits/dark.fits"
+
+CAM1_TMP_ROTATED="/storage/web/cam1_tmp_rotated.tiff"
+
+FITS_HEADER_DATA_FILE='/storage/fits/fits_header.dat'
 
 process=`ps ax | grep qhy_camera | grep -v grep | wc -l`
 
@@ -21,13 +27,14 @@ if [ $start_checker_exit_code -eq 0 ]; then
 	fi
 
 	if [ ! -f ${CAM1_DARK_IMAGE} ]; then
+		echo "Shooting dark frame "${CAM1_DARK_IMAGE}
+
 		/opt/allsky/bin/iris_control c
 
-		/opt/allsky/bin/qhy_camera -m "qhy5ii" -e 30000 -g 30 -o ${CAM1_DARK_IMAGE}
+		/opt/allsky/bin/qhy_camera_new -m "qhy5ii" -e 30000 -g 30 -o ${CAM1_DARK_IMAGE}
 	fi
 
 	/opt/allsky/bin/iris_control o
-
 else
 	if [ -f ${CAM1_CHECK_RUN_FILE} ]; then
 		exit 1
@@ -64,28 +71,72 @@ current_date_time=`date +"%d.%m.%Y %H:%M" `
 
 last_skytemp=`echo "select * from cloud_sensor order by time desc limit 1;" | mysql --connect-timeout=10 -uallsky -pallsky allsky -h 192.168.8.1 | tail -n1 | awk '{print $3}'`
 last_skytemp=`bc <<< "scale=2; ${last_skytemp}/1"`
+last_skytemp=`printf "%.2f\n" ${last_skytemp}`
+
+echo -e "\nCurrent environment conditions:"
+echo "    Temperature: "${last_temp}
+echo "    Humidity: "${last_humidity}
+echo "    Sky temperature: "${last_skytemp}
 
 rm -f /storage/web/cam1_tmp.jpg
 
-#-e 30000 -g 25
-
 optimal_shooting_params=`/opt/allsky/bin/ephem/get_optimal_night_cam_params.py`
 
-/opt/allsky/bin/qhy_camera -m "qhy5ii" ${optimal_shooting_params} -o /storage/web/cam1_tmp.jpg -k ${CAM1_DARK_IMAGE}
-#/opt/allsky/bin/qhy_camera -m "qhy5ii" -e 30000 -g 30 -o /storage/web/cam1_tmp.jpg -k /storage/web/dark.jpg  #-k /storage/web/bias.jpg #-k /storage/web/dark.jpg -b 5
-#/opt/allsky/bin/qhy_camera -m "qhy5ii" -e 300 -g 5 -o /storage/web/cam1_tmp.jpg -k /storage/web/dark.jpg
-#/opt/allsky/bin/qhy_camera -m "qhy5ii" -e 1000 -g 10 -o /storage/web/cam1_tmp.jpg -k /storage/web/dark.jpg
+echo -e "\nSelected camera exposure and gain: "${optimal_shooting_params}""
 
-#convert /storage/web/cam1_tmp.jpg -rotate 180 /storage/web/cam1_tmp_rotated.jpg
-convert /storage/web/cam1_tmp.jpg \( -rotate 180 \) \( -fill white -gravity West -pointsize 21 -annotate +5-220 'N' -gravity North -pointsize 21 \
-	-annotate +195+5 'W' -gravity East -pointsize 21 -annotate +5+190 'S' \) /storage/web/cam1_tmp_rotated.jpg
+rm -f ${FITS_HEADER_DATA_FILE}
+
+echo "Building FITS header data file "${FITS_HEADER_DATA_FILE}
+
+echo -e "CREATOR Allsky camera" > ${FITS_HEADER_DATA_FILE}
+echo -e "INSTRUME QHY5-IIM" >> ${FITS_HEADER_DATA_FILE}
+
+exposure_r=`echo ${optimal_shooting_params} | awk '{print $1}'`
+exposure_hr=`printf "%.1f\n" "$(bc <<< "scale=1; ${exposure_r}/1000")"`
+
+echo -e "EXPTIME "${exposure_hr} >> ${FITS_HEADER_DATA_FILE}
+echo -e "TEMPERAT "${last_temp} >> ${FITS_HEADER_DATA_FILE}
+echo -e "HUMIDITY "${last_humidity} >> ${FITS_HEADER_DATA_FILE}
+echo -e "SKYTEMP "${last_skytemp} >> ${FITS_HEADER_DATA_FILE}
+echo -e "OBSERVAT Crimean astrophysical observatory" >> ${FITS_HEADER_DATA_FILE}
+echo -e "SITENAME Nauchniy, Crimea" >> ${FITS_HEADER_DATA_FILE}
+echo -e "SITELAT 44.727007" >> ${FITS_HEADER_DATA_FILE}
+echo -e "SITELONG 34.013173" >> ${FITS_HEADER_DATA_FILE}
+echo -e "SITEELEV 600" >> ${FITS_HEADER_DATA_FILE}
+
+echo "Ok, file content:"
+cat ${FITS_HEADER_DATA_FILE}
+
+echo -e "\nCleanup..."
+mv -f ${CAM1_LAST_IMAGE}  ${CAM1_PREV_IMAGE}
+
+echo -e "\nStarting camera utility..."
+
+/opt/allsky/bin/qhy_camera_new -m "qhy5ii" ${optimal_shooting_params} -o ${CAM1_LAST_IMAGE} -k ${CAM1_DARK_IMAGE} -x ${FITS_HEADER_DATA_FILE}
+
+echo -e "\nRotating original image on 180 degree and applying SNWE overlay..."
+
+convert ${CAM1_LAST_IMAGE} \( +flop \) \( -fill white -gravity West -pointsize 21 -annotate +5-220 'N' -gravity North -pointsize 21 \
+	-annotate +195+5 'W' -gravity East -pointsize 21 -annotate +5+190 'S' \) ${CAM1_TMP_ROTATED}
+
+echo "Rotated image was saved as "${CAM1_TMP_ROTATED}
+
+echo "Applying weather overlay text and converting to jpg..."
 
 convert -background '#00000080' -fill white -size 1280x50 label:"Nauchniy - CAM1\nDate: ${current_date_time}  Temperature: ${last_temp} C  Humidity: ${last_humidity} %  Skytemp: ${last_skytemp} C"\
-	-gravity southwest /storage/web/cam1_tmp_rotated.jpg +swap -gravity south -set colorspace Gray -composite /storage/web/cam1_woverlay.jpg
+	-gravity southwest ${CAM1_TMP_ROTATED} +swap -gravity south -set colorspace Gray -composite -quality 85% /storage/web/cam1_woverlay.jpg
+
+echo "Moving the image to web interface"
 
 mv /storage/web/cam1_woverlay.jpg /storage/web/cam1.jpg
 
-rm /storage/web/cam1_tmp_rotated.jpg
+echo "Cleanup..."
+
+rm ${CAM1_TMP_ROTATED}
+
+echo ${CAM1_TMP_ROTATED}" was removed"
+
+echo "Archiving camera image"
 
 /opt/allsky/bin/cam_to_arch.sh cam1
 
--- scripts/cam_to_arch.sh
+++ scripts/cam_to_arch.sh
@@ -2,6 +2,26 @@
 
 CAM_NAME=$1
 
-# simple copy current image to archive
-cp -v /storage/web/${CAM_NAME}.jpg /storage/web/archive/`date +"%d.%m.%Y"`/${CAM_NAME}/`date +"%H.%M__%d.%m.%Y"`.jpg
-cp -v /storage/web/${CAM_NAME}.jpg /storage/web/archive/`date +"%d.%m.%Y"`/${CAM_NAME}/`date +"%H.%M__%d.%m.%Y"`.jpg
+FILE_EXTENSION=""
+ORIGINAL_FILE=""
+
+if [ "$CAM_NAME" = "cam1" ]; then
+	FILE_EXTENSION="fits"
+	ORIGINAL_FILE="/storage/fits/last.fits"
+else
+	FILE_EXTENSION=".jpg"
+	ORIGINAL_FILE="/storage/web/"${CAM_NAME}".jpg"
+fi
+
+NEW_FILE_NAME=`date +"%Y.%m.%dT%H.%M"`.${FILE_EXTENSION}
+DATE=`date +"%d.%m.%Y"`
+
+echo "Copying "${ORIGINAL_FILE} " to "/storage/web/${NEW_FILE_NAME}
+
+cp -v ${ORIGINAL_FILE} /storage/web/${NEW_FILE_NAME}
+
+echo "Upload FTP "
+
+ftp-upload -v -h 192.168.8.1 -u observer --password observer -d allsky/cam_img/${DATE}/${CAM_NAME} /storage/web/${NEW_FILE_NAME}
+
+rm /storage/web/${NEW_FILE_NAME}
